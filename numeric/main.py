ac5a6deabace5d1a349e37c68856de3c195cf68b
Srikanthkovuri <srikanthkovuri785@gmail.com>
2024-09-11 13:42:56 +0530

    added numeric api

--- numeric/main.py
+++ numeric/main.py
@@ -24,9 +24,18 @@ class NumericRes(BaseModel):
 
 app=FastAPI()
 
+@app.get("/")
+def hello():
+    """simple function
+
+    Returns:
+        str: string
+    """
+    return "Hey! Srikanth"
+
 @app.post("/add",response_model=NumericRes)
 def add(request: NumericReq):
-    """This function will return addition of values
+    """This function will return addition of 2 values
 
     Args:
         request (NumericReq): class
@@ -35,6 +44,48 @@ def add(request: NumericReq):
         value_1=request.value_1,
         value_2=request.value_2,
         result=request.value_1 + request.value_2
+    )
+    return response
+
+@app.post("/sub",response_model=NumericRes)
+def sub(request: NumericReq):
+    """This function will return subtraction of 2 values
+
+    Args:
+        request (NumericReq): class
+    """
+    response=NumericRes(
+        value_1=request.value_1,
+        value_2=request.value_2,
+        result=request.value_1 - request.value_2
+    )
+    return response
+
+@app.post("/mul",response_model=NumericRes)
+def mul(request: NumericReq):
+    """This function will return multiplication of 2 values
+
+    Args:
+        request (NumericReq): class
+    """
+    response=NumericRes(
+        value_1=request.value_1,
+        value_2=request.value_2,
+        result=request.value_1 * request.value_2
+    )
+    return response
+
+@app.post("/div",response_model=NumericRes)
+def div(request: NumericReq):
+    """This function will return division of 2 values
+
+    Args:
+        request (NumericReq): class
+    """
+    response=NumericRes(
+        value_1=request.value_1,
+        value_2=request.value_2,
+        result=request.value_1 / request.value_2
     )
     return response
 
